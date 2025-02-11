1f288e7467f0afb7b0423f02f2a2a7df13261db4
diegorbb <diegomairenna@gmail.com>
2025-02-11 22:44:34 +0000

    changes
    made some changes to the views

--- app/views.py
+++ app/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.db.models import Max, F, Value, Case, When
 from django.contrib.auth.decorators import login_required
-# from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from decimal import Decimal
